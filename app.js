f50b07f79bbbfaf19b79e79eea320e0b6a793b19
Azmaan Ahmad Khan <71922726+azmaanism@users.noreply.github.com>
2022-01-18 02:35:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function (){
-  // get randomnumber between 0-3
+btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = color[randomNumber];
+  color.textContent = colors[randomNumber];
 });
 
-function getRandomNumber(){
+function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 }
